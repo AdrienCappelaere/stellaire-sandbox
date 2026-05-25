643a7c669ef116b89c1c118a313fbd3f8540366f
Adrien Henna <47740396+AdrienCappelaere@users.noreply.github.com>
2026-05-25 16:45:35 +0200

    Update integrations.js

--- site/assets/js/integrations.js
+++ site/assets/js/integrations.js
@@ -19,7 +19,7 @@
 // 1. Crée ton conteneur GTM (web). Récupère ton ID au format GTM-XXXXXXX.
 // 2. Remplace GTM-XXXXXXX ci-dessous par ton ID. Commit. C'est tout.
 (function (w, d, s, l, i) {
-  if (i === 'GTM-TZHVRDHT') return; // tant que pas configuré, on ne charge rien
+  if (i === 'GTM-XXXXXXXX') return; // tant que pas configuré, on ne charge rien
   w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
   var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
   j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
